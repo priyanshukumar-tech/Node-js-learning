40e49f9e361a6a080117d8c0e5b04e0072c30728
priyanshukumar-tech <priyanshukr012005@gmail.com>
2026-02-19 13:57:13 +0530

    any params same op

--- index4a.js
+++ index4a.js
@@ -1,13 +1,43 @@
+// import express from 'express';
+// const app=express();
+
+
+
+// app.get('/:num1/:num2',(req,res)=>{
+//     const n1=Number(req.params.num1);
+//     const n2=Number(req.params.num2);
+//     const sum=n1+n2;
+//     res.send(sum);
+
+// })
+// app.listen(8080,()=>{
+//     console.log("server running")
+// })
+
+
+// import express from 'express';
+// const app=express();
+
+// app.get('/x/:num1/y/:num2/z/:num3',(req,res)=>{
+//     const n1=Number(req.params.num1);
+//     const n2=Number(req.params.num2);
+//     const n3=Number(req.params.num3);
+//     const sum=n1+n2+n3;
+//     res.send(sum);
+
+// })
+// app.listen(8080,()=>{
+//     console.log("server running")
+// })
+
+
+
+
 import express from 'express';
 const app=express();
 
-
-
-app.get('/:num1/:num2',(req,res)=>{
-    const n1=Number(req.params.num1);
-    const n2=Number(req.params.num2);
-    const sum=n1+n2;
-    res.send(sum);
+app.get('/:a/:b/:c/:d',(req,res)=>{
+    res.send("Hello Student");
 
 })
 app.listen(8080,()=>{
