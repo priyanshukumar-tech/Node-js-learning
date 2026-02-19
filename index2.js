f9dc25e40648c066aac251ef783a1234b1304d9f
priyanshukumar-tech <priyanshukr012005@gmail.com>
2026-02-19 11:32:59 +0530

    route created

--- index2.js
+++ index2.js
@@ -3,4 +3,8 @@ const app=express();
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080")
-})
+});
+ app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("Response from server from for /")
+ })
