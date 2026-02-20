095f9bc4c4f5abd86a357d06469a83edf2dd97a9
priyanshukumar-tech <priyanshukr012005@gmail.com>
2026-02-20 11:08:27 +0530

    middleware

--- index8.js
+++ index8.js
@@ -5,7 +5,14 @@ app.listen((8080),()=>{
     console.log("server running")
 });
 
+const greet=(req,res,next)=>{
+    console.log("Hello World");
+    next();
+}
+app.use(greet);        //middleware function to log "Hello World" f
+
 app.use(express.json())  // to parse json data from request body
+
 
 let users=[
         {id:1,name:"student1",email:"student1@gmail.com"},
@@ -13,10 +20,13 @@ let users=[
         {id:3,name:"student3",email:"student3@gmail.com"}
     ]
 
+app.get('/',(req,res)=>{
+    res.json(users);
+})
 app.get('/:id',(req,res)=>{
     const user=users.find(user=>user.id===Number(req.params.id));
     if(user){
-        res.send(user);
+        res.json(user);
     }
 })
 
